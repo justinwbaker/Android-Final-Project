bc6a25bf96ef4e3cdf309427b1d72a641a484bc7
justin baker <justin.baker@easternstudent.ca>
2018-05-15 19:08:33 -0300

    hello
    hello

--- FinalProject/app/src/main/java/ca/easterndevelopers/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/ca/easterndevelopers/finalproject/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // hello cody
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
