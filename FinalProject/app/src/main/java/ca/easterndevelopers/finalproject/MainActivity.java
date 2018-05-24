9633ec846f285fac2ffbd7f175b61b1f318fd2d5
justin baker <justin.baker@easternstudent.ca>
2018-05-24 12:19:14 -0300

    saving data test

--- FinalProject/app/src/main/java/ca/easterndevelopers/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/ca/easterndevelopers/finalproject/MainActivity.java
@@ -1,7 +1,9 @@
 package ca.easterndevelopers.finalproject;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,10 +21,21 @@ public class MainActivity extends Activity implements View.OnClickListener {
     private static int tilesInWidth = 20;
     public static boolean friendlyFire = true;
 
+    private static SharedPreferences localPrefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // loading data
+        localPrefs = this.getSharedPreferences("ca.easterndevelopers.finalproject", Context.MODE_PRIVATE);
+
+        /* test saving and loading data
+        load--> String data = localPrefs.getString("name", "unknown");
+        System.out.println(data);
+        save--> localPrefs.edit().putString("name", "justin").apply();
+        */
+
+        // display size and stuff
         Display display = getWindowManager().getDefaultDisplay();
         // Load the resolution into a Point object
         resolution = new Point();
@@ -48,5 +61,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
     //calculates actual size of tiles based on resolution
     public static float getTileSize() {
         return (resolution.x / tilesInWidth) * zoom;
+    }
+
+    public static SharedPreferences getLocalPrefs() {
+        return localPrefs;
     }
 }
