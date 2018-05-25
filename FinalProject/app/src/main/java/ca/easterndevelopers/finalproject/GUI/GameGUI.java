fcb5ea176ce1dae9f3b73f46a0404f1e4a29a21d
CodyJCormier <33677220+CodyJCormier@users.noreply.github.com>
2018-05-25 18:38:44 -0300

    Added is players turn lines

--- FinalProject/app/src/main/java/ca/easterndevelopers/finalproject/GUI/GameGUI.java
+++ FinalProject/app/src/main/java/ca/easterndevelopers/finalproject/GUI/GameGUI.java
@@ -8,6 +8,7 @@ import android.graphics.Rect;
 import ca.easterndevelopers.finalproject.MainActivity;
 import ca.easterndevelopers.finalproject.MainScreen;
 import ca.easterndevelopers.finalproject.game.Game;
+import ca.easterndevelopers.finalproject.level.LevelManager;
 import ca.easterndevelopers.finalproject.renderer.GameRenderer;
 
 public class GameGUI extends GUI {
@@ -32,20 +33,26 @@ public class GameGUI extends GUI {
             int x = GameRenderer.getTouchedPoint().x;
             int y = GameRenderer.getTouchedPoint().y;
 
+
+
             if(Rect.intersects(viewMap, new Rect(x, y, x+1, y+1))){
                 isOnGUI = true;
                 Game.setIsLookingAtMap(!Game.isLookingAtMap());
             }
 
-            if(Rect.intersects(nextUnit, new Rect(x, y, x+1, y+1))){
-                isOnGUI = true;
-                MainScreen.getPlayer().setNextActiveUnit();
-                // select next unit
-            }
+            if(MainScreen.getPlayer().isPlayersTurn()){
 
-            if(Rect.intersects(endTurn, new Rect(x, y, x+1, y+1))){
-                isOnGUI = true;
-                MainScreen.getPlayer().getCurrentLevel().endPlayersTurn();
+                if(Rect.intersects(nextUnit, new Rect(x, y, x+1, y+1))){
+                    isOnGUI = true;
+                    MainScreen.getPlayer().setNextActiveUnit();
+                    // select next unit
+                }
+
+                if(Rect.intersects(endTurn, new Rect(x, y, x+1, y+1))) {
+                    isOnGUI = true;
+                    MainScreen.getPlayer().getCurrentLevel().endPlayersTurn();
+
+                }
                 // go to enemy's turn
             }
         }
@@ -56,13 +63,21 @@ public class GameGUI extends GUI {
         canvas.restore();
         paint.setColor(Color.argb(135, 0, 0, 255));
         canvas.drawRect(viewMap, paint);
-        canvas.drawRect(nextUnit, paint);
-        canvas.drawRect(endTurn, paint);
+
         paint.setColor(Color.BLACK);
         paint.setTextSize(50);
         canvas.drawText("  MAP ", (int)MainActivity.getTileSize() + (int)MainActivity.getTileSize()/8, (int)MainActivity.getTileSize()*1.7f, paint);
-        canvas.drawText(" NEXT ", nextUnit.left + MainActivity.getTileSize()/2, nextUnit.bottom - MainActivity.getTileSize()/2, paint);
-        canvas.drawText("  END ", endTurn.left + MainActivity.getTileSize()/2, endTurn.bottom - MainActivity.getTileSize()/2, paint);
+
+        if(MainScreen.getPlayer().isPlayersTurn()) {
+
+            paint.setColor(Color.argb(135, 0, 0, 255));
+            canvas.drawRect(nextUnit, paint);
+            canvas.drawRect(endTurn, paint);
+            paint.setColor(Color.BLACK);
+            canvas.drawText(" NEXT ", nextUnit.left + MainActivity.getTileSize() / 2, nextUnit.bottom - MainActivity.getTileSize() / 2, paint);
+            canvas.drawText("  END ", endTurn.left + MainActivity.getTileSize() / 2, endTurn.bottom - MainActivity.getTileSize() / 2, paint);
+        }
+
         if(Game.debug)
             canvas.drawText("" + fps, MainActivity.getResolution().x/2 , MainActivity.getTileSize(), paint);
     }
