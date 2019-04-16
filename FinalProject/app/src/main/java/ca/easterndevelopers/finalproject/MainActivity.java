aa2d69eebb55eaca41ce4b52d0ab404f30127afa
justin baker <justin.baker@easternstudent.ca>
2019-04-16 15:14:58 -0300

    zipped project

--- FinalProject/app/src/main/java/ca/easterndevelopers/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/ca/easterndevelopers/finalproject/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     private static Point resolution;
     //for tile size calculations
-    private static float zoom = 0.3f;
+    private static float zoom = 1f;
     private static int tilesInWidth = 20;
     public static boolean friendlyFire = true;
 
